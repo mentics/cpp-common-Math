2cf146143bbce9d907462b922c83afe74b6a4d31
jshellman <joel@mentics.com>
2017-10-17 04:09:00 -0700

    EnterOrbit now works with Eigen and vector constraint function

---
+++ include/MenticsMath.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "nlopt/nlopt.hpp"
+
+//class Optimizer {
+//public:
+//	Optimizer(int numParams) : opt(nlopt::LD_SLSQP, numParams) {
+//
+//	}
+//
+//private:
+//	nlopt::opt opt;
+//};
