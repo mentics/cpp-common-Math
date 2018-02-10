9b7d550b7fc1854d1d1ec615bab38edba2e1de1f
Antonio Trpeski <antuan.trpeski@hotmail.gr>
2018-02-10 19:41:11 +0200

    moved functions to cpp files

--- include/MenticsMath.h
+++ include/MenticsMath.h
@@ -23,21 +23,6 @@ typedef void(*mfunc2)(unsigned m, double *result, unsigned n, const double* x, d
 
 typedef double(*calcError)(const std::vector<double> &x, void *data);
 
-
-inline bool isSimilar(double x1, double x2, double eps) {
-	const double sum = x1 + x2;
-	if (sum < eps) {
-		return true;
-	}
-	else {
-		return abs(x1 - x2) / sum < eps;
-	}
-}
-
-inline vect3 randomVector(double scale) {
-	return vect3(nextDouble() * scale, nextDouble() * scale, nextDouble() * scale);
-}
-
 void testGrad(std::string name, int m, mfunc2 f, std::vector<double> at, double dx, double eps, void* data);
 void testGrad(std::string name, vfunc2 f, std::vector<double> &at, double dx, double eps, void* data);
 
