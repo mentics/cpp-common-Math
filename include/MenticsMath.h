dfd76ccb81cc87c85e838a497c1173a8549ae7d9
Antonio Trpeski <antuan.trpeski@hotmail.gr>
2018-02-18 12:50:53 +0200

    updated tests

--- include/MenticsMath.h
+++ include/MenticsMath.h
@@ -9,10 +9,10 @@ namespace MenticsGame {
 extern std::function<double()> nextDouble;
 
 template <int SIZE>
-using vector = Eigen::Matrix<double, SIZE, 1>;
+using Evector = Eigen::Matrix<double, SIZE, 1>;
 using vect3 = Eigen::Vector3d;
 using vect4 = Eigen::Vector4d;
-using vect8 = vector<8>;
+using vect8 = Evector<8>;
 using mat3x8 = Eigen::Matrix<double, 3, 8>;
 
 typedef double(*vfunc2)(const std::vector<double> &x, std::vector<double> &grad, void *data);
