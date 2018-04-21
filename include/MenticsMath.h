49e735c570ca12cefd84a941ec6c7123c8e3a316
mentics <joel@mentics.com>
2018-04-21 10:31:23 -0700

    Changed some class templates to have TimeType first and started working on adding Actions to Agent

--- include/MenticsMath.h
+++ include/MenticsMath.h
@@ -5,20 +5,20 @@
 
 namespace MenticsGame {
 
-extern std::function<double()> nextDouble;
+typedef double CoordType;
+typedef double DistType;
 
 template <int SIZE>
-using Evector = Eigen::Matrix<double, SIZE, 1>;
+using Evector = Eigen::Matrix<CoordType, SIZE, 1>;
 using vect3 = Eigen::Vector3d;
 using vect4 = Eigen::Vector4d;
 using vect8 = Evector<8>;
-using mat3x8 = Eigen::Matrix<double, 3, 8>;
+using mat3x8 = Eigen::Matrix<CoordType, 3, 8>;
+
+extern std::function<double()> nextDouble;
 
 typedef double(*vfunc2)(const std::vector<double> &x, std::vector<double> &grad, void *data);
 typedef void(*mfunc2)(unsigned m, double *result, unsigned n, const double* x, double* grad, void* f_data);
-
-typedef double(*calcError)(const std::vector<double> &x, void *data);
-
 
 inline bool isSimilar(double x1, double x2, double eps) {
 	const double sum = x1 + x2;
